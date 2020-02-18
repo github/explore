473bf0df7344455281d5a47c884f48272b9ea152
Justin Kenyon <kenyonj@github.com>
2020-02-18 11:18:33 -0500

    Update topics_test.rb

--- test/topics_test.rb
+++ test/topics_test.rb
@@ -335,7 +335,7 @@ describe "topics" do
 
           match = line.match(/\b(\w+)\s\d[.,;:\s]/)
           if match
-            allowed_words_before_numbers = %w[Perl Pi Auth Vision]
+            allowed_words_before_numbers = %w[Perl Pi Auth Vision Java]
             assert_includes allowed_words_before_numbers, match[1],
                             'Write out "one" and every number less than 10, except when they ' \
                             "follow one of: #{allowed_words_before_numbers.join(', ')}"
